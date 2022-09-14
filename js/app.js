7a57fb48daa495ffe6732483390f8e6bc1d38c82
J M Cabrera Elias <jmcabrerae@gmail.com>
2022-09-14 11:23:45 -0700

    fine tuned java

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 
 console.log('HELLO WORLD');
 
-let userAskName = prompt('Greetings and Salutations Codefellows Citizen!' + ' What is your name?');
+let userAskName = prompt('Greetings and Salutations Codefellows Citizen!' + ' What is your name?').toLowerCase();
 
 alert('WELCOME TO THE JUNGLE ' + userAskName + '!!!!!');
 
-let askQuestion1 = prompt(' Do you want to play a game? ' + userAskName);
+let askQuestion1 = prompt(' Do you want to play a game? ' + userAskName).toLowerCase();
 
-alert('HINT- It does not matter what you want, WE ARE PLAYING!!! BUWAHAHAHAHA!!!');
+alert('HINT- It does not matter what you want, WE ARE PLAYING!!! BUWAHAHAHAHA!!! You MUST answer Yes or No.');
 
-let userQuestion1 = prompt('is my name Joey?');
+let userQuestion1 = prompt('is my name Joey?').toLowerCase();
 if (userQuestion1 === 'y' || userQuestion1 === 'yes') {
   alert('You are correct!');
 }
@@ -18,7 +18,7 @@ else if (userQuestion1 === 'n' || userQuestion1 === 'no') {
   alert('Sorry, you are WRONG');
 }
 
-let userQuestion2 = prompt('Do I have 3 sons?');
+let userQuestion2 = prompt('Do I have 3 sons?').toLowerCase();
 if (userQuestion2 === 'y' || userQuestion2 === 'yes') {
   alert('You are correct!');
 }
@@ -34,7 +34,7 @@ else if (userQuestion3 === 'n' || userQuestion3 === 'no') {
   alert('Sorry, you are WRONG');
 
 }
-let userQuestion4 = prompt('I am a military veteran?');
+let userQuestion4 = prompt('I am a military veteran?').toLowerCase();
 if (userQuestion4 === 'y' || userQuestion4 === 'yes') {
   alert('You are correct!');
 }
@@ -42,7 +42,7 @@ else if (userQuestion4 === 'n' || userQuestion4 === 'no') {
   alert('Sorry, you are WRONG');
 }
 
-let userQuestion5 = prompt('Did I live in Africa?');
+let userQuestion5 = prompt('Did I live in Africa?').toLowerCase();
 if (userQuestion5 === 'n' || userQuestion5 === 'no') {
   alert('You are correct!');
 }
